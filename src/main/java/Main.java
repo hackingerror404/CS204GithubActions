daa2673e7a7bf47bb45f82bf6745de9dd8a4effe
Jayden Hacking <hackingerror404@gmail.com>
2023-10-20 22:33:50 -0600

    Add main input system

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("exit")) {
+                break;
+            }
+
+            String[] parts = command.split(" ");
+            int a = Integer.parseInt(parts[1]);
+
+            try {
+                int b = Integer.parseInt(parts[2]);
+
+                switch (parts[0]) {
+                    case "add":
+                        System.out.println(calculator.add(a, b));
+                        break;
+                    case "subtract":
+                        System.out.println(calculator.subtract(a, b));
+                        break;
+                    case "multiply":
+                        System.out.println(calculator.multiply(a, b));
+                        break;
+                    case "divide":
+                        System.out.println(calculator.divide(a, b));
+                        break;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                switch (parts[0]) {
+                    case "fibonacci":
+                        System.out.println(calculator.fibonacciNumberFinder(a));
+                        break;
+                    case "binary":
+                        System.out.println(calculator.intToBinaryNumber(a));
+                        break;
+                }
+            }
+
+        }
     }
 }
